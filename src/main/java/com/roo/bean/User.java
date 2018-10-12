b07bc697f28e5fd067a513f3ee535741ddd20c59
yangsheng <1821332070@qq.com>
2018-10-12 09:50:48 +0800

    前端实体类文件目录:bean

---
+++ src/main/java/com/roo/bean/User.java
@@ -0,0 +1,32 @@
+package com.roo.bean;
+
+public class User {
+	private long id;
+	private String loginNm;
+	private String loginPwd;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getLoginNm() {
+		return loginNm;
+	}
+
+	public void setLoginNm(String loginNm) {
+		this.loginNm = loginNm;
+	}
+
+	public String getLoginPwd() {
+		return loginPwd;
+	}
+
+	public void setLoginPwd(String loginPwd) {
+		this.loginPwd = loginPwd;
+	}
+
+}
