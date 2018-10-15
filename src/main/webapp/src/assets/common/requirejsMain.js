ce0d274f14dc444fb9e358799c4de62606a7ec68
yangsheng <1821332070@qq.com>
2018-10-15 15:29:54 +0800

    require.js注释掉多余

--- src/main/webapp/src/assets/common/requirejsMain.js
+++ src/main/webapp/src/assets/common/requirejsMain.js
@@ -1,33 +1,28 @@
-function getReferrerUrl(){
-    if(window.name.indexOf("crossAPI")!=-1){
-            return window.name
-    }else{
-        var returnUrl;
-        if(document.referrer){
-            var urlArr=document.referrer.split("//");
-            returnUrl= urlArr[0]+"//"+urlArr[1].split("/")[0]+'/'
-            +urlArr[1].split("/")[1].split("/")[0]+
-            '/src/assets/common/crossAPI';
-        }else{
-            returnUrl= "https://192.168.100.36:8843/ngcs/src/assets/common/crossAPI"
-        }
-        returnUrl = "assets/common/crossAPI";
-        window.name=returnUrl;
-        return returnUrl;
-    }
-}
-
+/**
+ * @auth  yangsheng
+ * @des   require.js
+ * @date  2018-10-12
+ * @des   重写requirejsMain
+ */
 var require = {
 	baseUrl : "../../",
     map: {
-      	'*': { 'style': 'assets/lib/requirejs/css.min' }
+      	/*'*': { 'style': 'assets/lib/requirejs/css.min' }*/
     },
 	paths : {
-		/*
-		*	避免js文件名和映射名相同，防止打包时被当做js文件重命名
-		*/
-        // // 如需引入crossAPI可放开下方引用 
-        'crossAPI':getReferrerUrl(),
+        //jquery
+		'jquery' : 'assets/lib/jquery/jquery',
+		
+		//ajax
+		//'ajax' : 'assets/common/ajax_amd',
+			
+		//event
+		//'event' : 'assets/common/event'
+		
+		
+		
+		//统一注释掉
+		/*'crossAPI':getReferrerUrl(),
         'jquery' : 'assets/lib/jqr/jqr',
         'backbone':'assets/lib/backbone/a',
 		'laydate':'assets/lib/laydate/laydate',
@@ -37,17 +32,18 @@ var require = {
         'simpleTree':'assets/components/simpleTree/simpleTree',
         'ueditorConfig':'assets/lib/ueditor/ueditor.config',
         'ueditor':'assets/lib/ueditor/ueditor.all',
-        'echarts': 'assets/lib/echarts/echarts-all',
+        'echarts': 'assets/lib/echarts/echarts-all',*/
+        
         //jquery plugins begin---
-        'jquery.jplayer':'assets/lib/jqueryPlugin/jPlayer/dist/jplayer/jquery.jplayer',
+        /*'jquery.jplayer':'assets/lib/jqueryPlugin/jPlayer/dist/jplayer/jquery.jplayer',
         "jquery.placeholder": "assets/lib/jqueryPlugin/placeholder/jquery.placeholder.min",
         "jquery.fileuploader": "assets/lib/jqueryPlugin/jQuery-File-Upload/js/jquery.fileupload",
         "jquery.ui.widget":"assets/lib/jqueryPlugin/jQuery-File-Upload/js/vendor/jquery.ui.widget",
         "jquery.pagination":"assets/lib/jqueryPlugin/pagination/jquery.pagination",
-        'jqueryUI': 'assets/lib/jqueryui/jquery-ui.min',
+        'jqueryUI': 'assets/lib/jqueryui/jquery-ui.min',*/
         //jquery plugins end---
 
-        'cookie' : 'assets/common/cookie',
+        /*'cookie' : 'assets/common/cookie',
         'ajax' : 'assets/common/ajax_amd',
         'hdb' : 'assets/lib/handlebars/handlebars',
         'hdbr' : 'assets/lib/handlebars/handlebars.runtime',
@@ -58,9 +54,10 @@ var require = {
         'underscore':'assets/lib/underscore/underscore',
         'json2' : 'assets/lib/json2/json2',
         'type' : 'assets/common/type',
-        'event' : 'assets/common/event',
+        'event' : 'assets/common/event',*/
+		
         /*组件*/
-		'tab':'assets/components/tab/tab',
+		/*'tab':'assets/components/tab/tab',
         'radios':'assets/components/radios/radios',
 		'list':'assets/components/list/list',
 		'select':'assets/components/select/select',
@@ -87,12 +84,15 @@ var require = {
         'loggerParam' : 'js/loggerParam',
         'loggerNgbusi' : 'js/loggerNgbusi',
         'jqComboSelect':'assets/lib/comboSelect/jquery.combo.select',
-        'comboSelect':'assets/components/comboSelect/comboSelect'
+        'comboSelect':'assets/components/comboSelect/comboSelect'*/
 
     },
 	waitSeconds:0,
 	shim:{
-        'jquery.fileuploader': { deps: ['jquery.ui.widget'] },
+		//'ajax': { deps: ['jquery'] }
+		
+		//统一注释掉
+        /*'jquery.fileuploader': { deps: ['jquery.ui.widget'] },
         'jquery.fileuploader': { deps: ['jquery'] },
         'jquery.ui.widget': { deps: ['jquery'] },
         'jquery.placeholder': { deps: ['jquery'] },
@@ -106,13 +106,6 @@ var require = {
         'ajax': { deps: ['jquery'] },
         'artDialog': { deps: ['jquery'] },
         'zTree' : { deps:['jquery'], exports:'$.fn.zTree' },
-        'jqueryUI': { deps: ['jquery'] }
-	},
-    urlArgs: function(id, url) {
-        if (url.match("\.tpl$")||url.match("\.js$")||url.match("\.css$")) {
-            return '?v='+(window.Args?Args:Math.random());
-        }
-        return '';
-    }
-
+        'jqueryUI': { deps: ['jquery'] }*/
+	}
 }
