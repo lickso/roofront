95176e5c9462644e0676ebf41c7d51a7aff27010
yangsheng <1821332070@qq.com>
2018-10-15 15:37:25 +0800

    java-测试类

---
+++ src/main/java/com/roo/util/Test.java
@@ -0,0 +1,13 @@
+package com.roo.util;
+
+public class Test {
+	public static void main(String[] args) {
+		/*double total = 0.00;
+		total += Double.parseDouble("1.01");
+		total += Double.parseDouble("1.02");
+		System.out.println(total);*/
+		
+		double avgThreemonthFlowDouble = (double)6032/3/1024;
+		System.out.println(avgThreemonthFlowDouble);
+	}
+}
