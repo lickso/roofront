a5b37db1164b60e4017fa8b83aa9bd38065d75b4
yangsheng <1821332070@qq.com>
2018-10-16 10:22:29 +0800

    本地静态

--- src/main/java/com/roo/util/LocalConstants.java
+++ src/main/java/com/roo/util/LocalConstants.java
@@ -1,11 +1,24 @@
 package com.roo.util;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @des    静态常量
  * @date   2018-10-12
  * @author yangsheng
  *
  */
-public final class Constants {
+public final class LocalConstants {
+	
+	//前端系统异常枚举
+	private static final Map<Integer,String>  ROO_SYS_ERROR_MAPPER = new HashMap<Integer,String>();
+	static{
+		ROO_SYS_ERROR_MAPPER.put(201, "前端异常");
+		ROO_SYS_ERROR_MAPPER.put(202, "前端入参异常");
+	}
+	public static String getSysErrorValue(int code) {
+		return ROO_SYS_ERROR_MAPPER.get(code);
+	}
 	
 }
